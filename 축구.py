1bf863cb7a4b9300bfc4d473e003067cdded7e78
penguin1023 <2530907@sw.hs.kr>
2025-08-22 14:36:20 +0900

    축구.py

--- 축구.py
+++ 축구.py
@@ -11,7 +11,7 @@ team = st.text_input("응원팀을 입력하세요")
 
 player = st.text_input("좋아하는 선수를 입력하세요")
 
-table = st.number_input("리그 예상 순위를 입력하세요")
+table = st.text_input("리그 예상 순위를 입력하세요")
 
 trophy = st.selectbox("우승 트로피를 고르세요", ["리그", "리그컵", "챔스", "무관"])
 
@@ -25,7 +25,7 @@ if player:
   st.write(f"최애선수: {player}")
 
 if table:
-  st.write(f"순위: {int(table)}")
+  st.write(f"순위: {table}")
 
 if trophy:
   st.write(f"트로피: {trophy}")
